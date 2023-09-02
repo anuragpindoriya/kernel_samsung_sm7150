7aaa43d777ba1a36fbf0d4c70c220ddc49cfd396
iambinaytiwari <40681532+iambinaytiwari@users.noreply.github.com>
2023-09-02 06:52:01 +0545

    AnyKernel3: import for our device
    Signed-off-by: iambinaytiwari <binaytiwari6@gmail.com>

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,50 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=M51 Kernel For Android 10-13
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=10-16
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+# dtboblock=/dev/block/bootdevice/by-name/dtbo;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+
+## AnyKernel install
+dump_boot;
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+write_boot;
+## end install
